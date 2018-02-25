3fd4a4981e524a27e18de7b6fd2711a56cd79f19
ajDon <ajays3370@gmail.com>
2018-02-25 16:41:30 +0530

    Enable Script and Content Editor Webparts

---
+++ PS Scripts/Enable Script and Content Editor Webpart.ps1
@@ -0,0 +1,4 @@
+﻿$adminUPN=”ajays3370@ajaysahu.onmicrosoft.com”
+$userCredential = Get-Credential -UserName $adminUPN -Message “Enter password”
+Connect-SPOService -Url https://ajaysahu-admin.sharepoint.com -Credential $userCredential
+Set-SPOsite https://ajaysahu.sharepoint.com -DenyAddAndCustomizePages 0
